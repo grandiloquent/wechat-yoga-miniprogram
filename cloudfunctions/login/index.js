2d0b4eb35ffab21c472dea9cd27f26eaa7bdf0c3
grandiloquent <1785907865@qq.com>
2022-07-25 05:38:26 +0800

    提供在线示例的二维码

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -1,10 +1,7 @@
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
 
 cloud.init()
-//const db = cloud.database()
 
-// 云函数入口函数
 exports.main = async (event, context) => {
     return {
         openid: cloud.getWXContext().OPENID

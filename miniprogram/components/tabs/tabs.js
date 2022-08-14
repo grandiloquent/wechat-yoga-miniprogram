abde576fad37cf22c3bcb401111895ff66af5e5e
grandiloquent <1785907865@qq.com>
2022-08-14 11:49:19 +0800

    更新tabs组件

--- miniprogram/components/tabs/tabs.js
+++ miniprogram/components/tabs/tabs.js
@@ -1,20 +1,43 @@
+// "tabs": "../../components/tabs/tabs"
+// <tabs></tabs>
 Component({
-  properties: {
-    selectedTabIndex: {
-      type: Number,
-      value: 0
-    }
-  },
-  data: {},
-  methods: {
-    onSelectedIndexChange(e) {
-      const index = e.currentTarget.dataset.index;
-      this.setData({
-        selectedTabIndex: index
-      });
-      this.triggerEvent('selectIndexChanged', index);
-      // bind:selectedIndexChange="onSelectedIndexChange"
+    properties: {
+        selected: {
+            type: Number,
+            value: 0
+        }
     },
-
-  }
+    data: {
+        weeks: [
+            '周一',
+            '周二',
+            '周三',
+            '周四',
+            '周五',
+            '周六',
+            '周日'
+        ]
+    },
+    pageLifetimes: {
+        show() {
+            this.swipeBorder();
+        }
+    },
+    methods: {
+        onTap(evt) {
+            this.setData({
+                selected: parseInt(evt.currentTarget.dataset.index)
+            })
+            this.swipeBorder();
+            this.triggerEvent('submit', evt.currentTarget.dataset.index)
+        },
+        swipeBorder() {
+            this.createSelectorQuery().select('.active').boundingClientRect().exec(res => {
+                this.setData({
+                    width: `width:${res[0].width}px;`,
+                    left: `left:${res[0].left}px;`
+                })
+            })
+        }
+    }
 })

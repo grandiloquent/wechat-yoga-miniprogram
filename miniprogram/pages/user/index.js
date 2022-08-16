c50a2051e45f5d74f6b2d469b5d4a18fb4b827f3
grandiloquent <1785907865@qq.com>
2022-08-16 15:26:13 +0800

    更新

--- miniprogram/pages/user/index.js
+++ miniprogram/pages/user/index.js
@@ -5,7 +5,94 @@ Page({
     data: {
         app
     },
-
+    async initialize() {
+        wx.request({
+            url: `${app.globalData.host}/api/accessRecords?path=${encodeURIComponent('/pages/user/index')}`
+        })
+        try {
+            await shared.fetchToken(app);
+        } catch (e) {
+            this.setData({
+                showLogin: true
+            });
+            return
+        }
+        await this.loadData();
+    },
+    async loadData() {
+        const obj = await fetchUser(app);
+        console.log(obj);
+        this.setData({
+            user: obj.user,
+            public: obj.books.filter(x => x.class_type === 4)[0].count
+        })
+    },
+    navigateTo(e) {
+        wx.navigateTo({
+            url: e.currentTarget.dataset.src
+        })
+    },
+    onGroupCourses(e) {
+        wx.switchTab({
+            url: '/pages/appointment/index'
+        })
+    },
+    async onLoad(options) {
+        shared.applyBasicSettings();
+        if (!app.globalData.configs) {
+            app.globalData.ready = () => {
+                this.setData({
+                    app
+                })
+                this.initialize();
+            }
+            return
+        }
+        await this.initialize();
+    },
+    async onLoginSuccess(res) {
+        this.setData({
+            showLogin: false
+        });
+        await this.initialize(this.data.id)
+    },
+    onMemberPhysicalExamination(e) {
+        wx.navigateTo({
+            url: `/pages/physicalExamination/physicalExamination`
+        });
+    },
+    onPrivateTeacherCourse(e) {
+        wx.navigateTo({
+            url: "/pages/coaches/index"
+        })
+    },
+    async onShow() {
+        this.setData({
+            backgroundColor: shared.getRandomColor(),
+        });
+    },
+    onShowFeedback(e) {
+        this.setData({
+            feedbackActive: true
+        });
+    },
+    onShowHistory() {
+        wx.navigateTo({
+            url: '/pages/lessons/lessons'
+        })
+    },
+    onShowVipList() {
+        wx.navigateTo({
+            url: '/pages/userVipList/userVipList'
+        })
+    },
+    onSuccess(res) {
+        app.globalData.userInfo = res.detail;
+        this.setData({
+            showLogin: false,
+            user: res.detail
+        })
+    }
 })
 
 function fetchUser(app) {

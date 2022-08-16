776c48cb9585580bc823aff1150779bce083b738
grandiloquent <1785907865@qq.com>
2022-08-16 15:51:55 +0800

    更新用户会员卡列表

--- miniprogram/pages/userVipList/userVipList.js
+++ miniprogram/pages/userVipList/userVipList.js
@@ -1,18 +1,85 @@
-const shared = require('../../shared');
 const app = getApp();
+const shared = require('../../shared')
+
 Page({
-  data: {
-    app,
-  },
-  onLoad(options) {
-    this.data.id = options.id || 499;
-    shared.execute(this, "vipCard/2", [
-      "userId", this.data.id
-    ], data => {
-      this.setData({
-        "vipCards": data
-      })
-      console.log(data)
-    });
-  }
+    data: {
+        active: false,
+        selectedDateTime: 0,
+        offsetDays: 0,
+        app,
+    },
+    // 开始加载数据和渲染页面
+    async initialize() {
+        wx.request({
+            url: `${app.globalData.host}/api/accessRecords?path=${encodeURIComponent('/pages/appointment/index')}`
+        })
+        try {
+            await shared.fetchToken(app);
+        } catch (e) {
+            this.setData({
+                showLogin: true
+            });
+            return
+        }
+        await this.loadData();
+    },
+    async loadData() {
+        let res;
+        try {
+            res = await fetch(app)
+            this.setData({
+                items: res.map(x => {
+                    x.second = `剩余 ${(x.end_date - (new Date().setHours(0, 0, 0, 0) / 1000)) / 86400} 天`
+                    x.subhead = `已使用 ${x.count || 0} 次`
+                    return x;
+                })
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    },
+    // 加载程序配置、应用基础页面配置、启动数据渲染
+    async onLoad(options) {
+        shared.applyBasicSettings();
+        if (!app.globalData.configs) {
+            app.globalData.ready = () => {
+                this.setData({
+                    app
+                })
+                this.initialize();
+            }
+            return
+        }
+        await this.initialize();
+    },
+    async onLoginSuccess(res) {
+        this.setData({
+            showLogin: false
+        });
+        await this.initialize(this.data.id)
+    },
+    onShareAppMessage() {
+        return {
+            title: '晨蕴瑜伽日课表'
+        }
+    },
 })
+
+function fetch(app) {
+    return new Promise(((resolve, reject) => {
+        wx.request({
+            url: `${app.globalData.host}/api/user.query.vip?id=${app.globalData.userInfo.id}`,
+            header: {
+                token: app.globalData.token
+            },
+            success: res => {
+                resolve(res.data);
+            },
+            fail: err => {
+                reject(err)
+            }
+        })
+    }))
+}
+
+

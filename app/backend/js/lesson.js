f02bec2dbbea3d8b1cdea5661c8be05756e15435
grandiloquent <1785907865@qq.com>
2022-12-25 11:25:43 +0800

    更新后端

--- app/backend/js/lesson.js
+++ app/backend/js/lesson.js
@@ -2,9 +2,22 @@ const suspendLesson = document.querySelector('#suspend-lesson');
 const id = new URL(document.URL).searchParams.get('id') || 415;
 const actions = document.querySelector('.actions');
 const image = document.querySelector('#image');
-const title = document.querySelector('#title');
+const title = document.querySelector('#title'); 
+const updateLesson = document.getElementById('update-lesson');
+//==================//
+
 const subheadText = document.querySelector('#subhead-text');
 
+updateLesson.addEventListener('click',updateLessonHandler);
+
+function updateLessonHandler(){
+}
+
+/*
+updateLesson.textContent='';
+updateLesson.setAttribute('class','');
+*/
+//==================//
 let baseUri = window.location.host === "127.0.0.1:5500" ? 'http://127.0.0.1:8081' : ''
 
 suspendLesson.addEventListener('click', suspendLessonHandler);

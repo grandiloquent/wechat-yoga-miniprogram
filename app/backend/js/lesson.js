dba7f31e05205aa1ca5a923fcaff6b149ac9b565
grandiloquent <1785907865@qq.com>
2022-12-25 19:47:09 +0800

    更新后端

--- app/backend/js/lesson.js
+++ app/backend/js/lesson.js
@@ -15,14 +15,54 @@ const pickerLessonType = document.getElementById('picker-lesson-type');
 const pickerTeacher = document.getElementById('picker-teacher');
 const pickerStartTime = document.getElementById('picker-start-time');
 const pickerPeoples = document.getElementById('picker-peoples');
+const submit = document.getElementById('submit');
+const popupClose = document.getElementById('popup-close');
 //==================//
 suspendLesson.addEventListener('click', suspendLessonHandler);
 updateLesson.addEventListener('click', updateLessonHandler);
 popupButtonBack.addEventListener('click', popupButtonBackHandler);
+
 function deleteBook(student) {}
 
 
 
+submit.addEventListener('click', submitHandler);
+
+function submitHandler(evt) {
+  evt.stopPropagation();
+  evt.stopImmediatePropagation();
+  popup.style.display = 'none';
+
+  const class_type = ((pickerLessonType.selectedItem === '小班') && 1) || ((pickerLessonType.selectedItem === '团课') && 4);
+  const lesson_name = pickerLesson.selectedItem;
+  const peoples = parseInt(pickerPeoples.selectedItem || '0');
+  const start_time = durationToSeconds(pickerStartTime.selectedItem + ":00");
+  const end_time = start_time + 3600;
+  const teacher_name = pickerTeacher.selectedItem;
+  console.log({
+    class_type,
+    lesson_name,
+    peoples,
+    start_time,
+    end_time,
+    teacher_name
+  });
+}
+
+
+popupClose.addEventListener('click', popupCloseHandler);
+
+function popupCloseHandler(evt) {
+  evt.stopPropagation();
+  evt.stopImmediatePropagation();
+  popup.style.display = 'none';
+}
+
+/*
+popupClose.textContent='';
+popupClose.setAttribute('class','');
+popupClose.style.display='block'
+*/
 //==================//
 
 

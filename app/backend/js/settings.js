5b602e99a4f2238aa544bbe3eced88f97c867168
grandiloquent <1785907865@qq.com>
2022-12-28 12:39:01 +0800

    更新服务器

--- app/backend/js/settings.js
+++ app/backend/js/settings.js
@@ -1,11 +1,43 @@
-setHeader();
-
-function setHeader() {
-  const customHeader = document.querySelector('custom-header');
-  customHeader.setAttribute('title', "管理员");
-  [...document.querySelectorAll('[data-href]')].forEach(x => {
-    x.addEventListener('click', evt => {
-      window.location = evt.currentTarget.dataset.href;
+document.querySelectorAll('[bind]').forEach(element => {
+  if (element.getAttribute('bind')) {
+    window[element.getAttribute('bind')] = element;
+  }
+  [...element.attributes].filter(attr => attr.nodeName.startsWith('@')).forEach(attr => {
+    if (!attr.value) return;
+    element.addEventListener(attr.nodeName.slice(1), evt => {
+      window[attr.value](evt);
     });
-  })
+  });
+})
+
+function showDrawer(evt) {
+  evt.stopPropagation();
+  customDrawer.setAttribute('expand', 'true');
 }
+let baseUri = window.location.host === "127.0.0.1:5500" ? 'http://127.0.0.1:8081' : ''
+async function loadData() {
+  const response = await fetch(`${baseUri}/v1/admin/notices`, {
+    headers: {
+      "Authorization": window.localStorage.getItem("Authorization")
+    }
+  })
+  return response.json();
+}
+async function render() {
+  const wrapper = document.querySelector('.wrapper');
+  let obj;
+  try {
+    obj = await loadData();
+    obj.forEach(value => {
+      const div = document.createElement('div');
+      div.textContent = value.title;
+      div.addEventListener('click', evt => {
+        evt.stopPropagation();
+      });
+      wrapper.appendChild(div);
+    })
+  } catch (error) {
+
+  }
+}
+render();

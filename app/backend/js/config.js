2690247eaecc460092a7fe32c3eda6f13d4cd006
grandiloquent <1785907865@qq.com>
2022-12-29 10:07:30 +0800

    更新

---
+++ app/backend/js/config.js
@@ -0,0 +1,15 @@
+const SETTINGS = {
+    title: '瑜伽约课微信小程序'
+};
+document.querySelectorAll('[bind]').forEach(element => {
+  if (element.getAttribute('bind')) {
+    window[element.getAttribute('bind')] = element;
+  }
+  [...element.attributes].filter(attr => attr.nodeName.startsWith('@')).forEach(attr => {
+    if (!attr.value) return;
+    element.addEventListener(attr.nodeName.slice(1), evt => {
+      window[attr.value](evt);
+    });
+  });
+})
+document.title = `${document.title} - ${SETTINGS.title}`;

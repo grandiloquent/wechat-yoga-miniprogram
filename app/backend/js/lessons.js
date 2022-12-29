4fa9b15146bdd91f8f6f08a1aa08745621807df2
grandiloquent <1785907865@qq.com>
2022-12-30 03:41:50 +0800

    更新

--- app/backend/js/lessons.js
+++ app/backend/js/lessons.js
@@ -1,18 +1,3 @@
-document.querySelectorAll('[bind]').forEach(element => {
-  if (element.getAttribute('bind')) {
-    window[element.getAttribute('bind')] = element;
-  }
-  [...element.attributes].filter(attr => attr.nodeName.startsWith('@')).forEach(attr => {
-    if (!attr.value) return;
-    element.addEventListener(attr.nodeName.slice(1), evt => {
-      window[attr.value](evt);
-    });
-  });
-})
-function showDrawer(evt) {
-  evt.stopPropagation();
-  customDrawer.setAttribute('expand', 'true');
-}
 
 function filterHandler(evt) {
   if (!evt.detail) return;

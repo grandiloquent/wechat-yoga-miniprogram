aa1ba27e4355145f50ffcc064dfda0550870c00e
grandiloquent <1785907865@qq.com>
2022-12-31 02:31:43 +0800

    更新日期选择控件

--- app/components/custom-date-picker.js
+++ app/components/custom-date-picker.js
@@ -12,10 +12,7 @@ export class CustomDatePicker extends LitElement {
     _pad: {},
     selectedDate: {}
   };
-  static styles = css`
-.next{
-grid-column:  7 / 8;
-}.pre{grid-column:1 / 2;}.nav{height:24px;width:100%;display:flex;align-items:center;justify-content:center;color:rgb(95,99,104);fill:currentColor;}svg{height:24px;width:24px;}.item{height:36px;width:36px;display:flex;align-items:center;justify-content:center;color:rgb(32,33,36);font-size:14px;line-height:20px;border-radius:100%}.item.selected{background:rgb(26,115,232);color:#fff;}.week{font-size:14px;line-height:20px;text-align:center;color:#333;color:rgb(95,99,104);height:36px;display:flex;align-items:center;justify-content:center;}.dialog-body{display:grid;grid-template-columns:repeat(7,1fr);gap:1px;padding:16px;justify-items:center;}.title{font-size:20px;line-height:24px;font-weight:500;margin:0px 0px 16px 0px;color:rgb(32,33,36);grid-column:3 / 6;text-align:center;}:host{color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:12px;z-index:1002;position:fixed;top:0;left:0;right:0;bottom:0;display:flex;align-items:center;justify-content:center;}.dialog{position:relative;z-index:1002;overflow-y:auto;color:#0f0f0f;width:90vw;border-radius:8px;background-color:#fff;overflow-y:auto;}.dialog::-webkit-scrollbar{display:none;}.overlay{font-family:Roboto,Arial,sans-serif;word-wrap:break-word;color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:1.2rem;position:fixed;top:0;bottom:0;left:0;right:0;z-index:1001;cursor:pointer;background-color:rgba(0,0,0,0.3);}`;
+  static styles = css`.button{height:36px;font-size:14px;line-height:36px;border-radius:18px;padding:0px 16px;}.button-blue{color:#fff;background-color:#065fd4}.next{grid-column:7 / 8;}.pre{grid-column:1 / 2;}.nav{height:24px;width:100%;display:flex;align-items:center;justify-content:center;color:rgb(95,99,104);fill:currentColor;}svg{height:24px;width:24px;}.item{height:36px;width:36px;display:flex;align-items:center;justify-content:center;color:rgb(32,33,36);font-size:14px;line-height:20px;border-radius:100%}.item.selected{background:rgb(26,115,232);color:#fff;}.week{font-size:14px;line-height:20px;text-align:center;color:#333;color:rgb(95,99,104);height:36px;display:flex;align-items:center;justify-content:center;}.dialog-body{display:grid;grid-template-columns:repeat(7,1fr);gap:1px;padding:16px 16px 0 16px;justify-items:center;}.title{font-size:20px;line-height:24px;font-weight:500;margin:0px 0px 16px 0px;color:rgb(32,33,36);grid-column:3 / 6;text-align:center;}:host{color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:12px;z-index:1002;position:fixed;top:0;left:0;right:0;bottom:0;display:flex;align-items:center;justify-content:center;}.dialog{position:relative;z-index:1002;overflow-y:auto;color:#0f0f0f;width:90vw;border-radius:8px;background-color:#fff;overflow-y:auto;}.dialog::-webkit-scrollbar{display:none;}.overlay{font-family:Roboto,Arial,sans-serif;word-wrap:break-word;color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:1.2rem;position:fixed;top:0;bottom:0;left:0;right:0;z-index:1001;cursor:pointer;background-color:rgba(0,0,0,0.3);}.buttons{display:flex;align-items:center;justify-content:flex-end;margin:12px 16px 16px;}`;
   constructor() {
     super();
     const now = new Date();
@@ -37,6 +34,9 @@ grid-column:  7 / 8;
       },
       (_, i) => new Date(year, month - 1, i + 1)
     )
+  }
+  close() {
+    this.remove();
   }
   render() {
     return html`<div class="dialog">
@@ -62,7 +62,16 @@ grid-column:  7 / 8;
     console.log(element,this.selectedDate);
     return html`<div class="item ${this.selectedDate===element.getTime()?'selected':''}" data-index="${element.getTime()}" @click="${this.navigate}">${element.getDate()}</div>`;
     })}
+
   </div>
+<div class="buttons">
+  <div class="button" @click=${this.close}>
+    取消
+  </div>
+ <div class="button button-blue" @click=${this._submit}>
+    确定
+  </div>
+</div>
 </div>
 <div class="overlay" @click="${this.close}">
 </div>`;

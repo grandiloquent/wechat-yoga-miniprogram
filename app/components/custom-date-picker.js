32cac9c9e3250f45903a41928e4b443abeab62d4
grandiloquent <1785907865@qq.com>
2022-12-31 02:18:21 +0800

    更新日期选择控件

--- app/components/custom-date-picker.js
+++ app/components/custom-date-picker.js
@@ -7,22 +7,29 @@ export class CustomDatePicker extends LitElement {
   static properties = {
     data: {},
     _weeks: {},
-    _date: {}
+    _date: {},
+    _now: {},
+    _pad: {},
+    selectedDate: {}
   };
-  static styles = css`.item{height:36px;width:36px;display:flex;align-items:center;justify-content:center;color:rgb(32,33,36);font-size:14px;line-height:20px;}.week{font-size:14px;line-height:20px;text-align:center;color:#333;color:rgb(95,99,104);}.dialog-body{display:grid;grid-template-columns:repeat(7,1fr);gap:1px;padding:16px;justify-items:center;}.title{font-size:20px;line-height:24px;font-weight:500;margin:0px 0px 16px 0px;color:rgb(32,33,36);grid-column:3 / 6;text-align:center;}:host{color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:12px;z-index:1002;position:fixed;top:0;left:0;right:0;bottom:0;display:flex;align-items:center;justify-content:center;}.dialog{position:relative;z-index:1002;max-height:180px;overflow-y:auto;color:#0f0f0f;width:90vw;border-radius:8px;background-color:#fff;overflow-y:auto;}.dialog::-webkit-scrollbar{display:none;}.overlay{font-family:Roboto,Arial,sans-serif;word-wrap:break-word;color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:1.2rem;position:fixed;top:0;bottom:0;left:0;right:0;z-index:1001;cursor:pointer;background-color:rgba(0,0,0,0.3);}`;
+  static styles = css`
+.next{
+grid-column:  7 / 8;
+}.pre{grid-column:1 / 2;}.nav{height:24px;width:100%;display:flex;align-items:center;justify-content:center;color:rgb(95,99,104);fill:currentColor;}svg{height:24px;width:24px;}.item{height:36px;width:36px;display:flex;align-items:center;justify-content:center;color:rgb(32,33,36);font-size:14px;line-height:20px;border-radius:100%}.item.selected{background:rgb(26,115,232);color:#fff;}.week{font-size:14px;line-height:20px;text-align:center;color:#333;color:rgb(95,99,104);height:36px;display:flex;align-items:center;justify-content:center;}.dialog-body{display:grid;grid-template-columns:repeat(7,1fr);gap:1px;padding:16px;justify-items:center;}.title{font-size:20px;line-height:24px;font-weight:500;margin:0px 0px 16px 0px;color:rgb(32,33,36);grid-column:3 / 6;text-align:center;}:host{color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:12px;z-index:1002;position:fixed;top:0;left:0;right:0;bottom:0;display:flex;align-items:center;justify-content:center;}.dialog{position:relative;z-index:1002;overflow-y:auto;color:#0f0f0f;width:90vw;border-radius:8px;background-color:#fff;overflow-y:auto;}.dialog::-webkit-scrollbar{display:none;}.overlay{font-family:Roboto,Arial,sans-serif;word-wrap:break-word;color:#0f0f0f;-webkit-text-size-adjust:100%;font-size:1.2rem;position:fixed;top:0;bottom:0;left:0;right:0;z-index:1001;cursor:pointer;background-color:rgba(0,0,0,0.3);}`;
   constructor() {
     super();
-
     const now = new Date();
+    this._now = now;
     this.data = this._getDays(now.getMonth() + 1, now.getFullYear());
     this._weeks = ['日', '一', '二', '三', '四', '五', '六'];
-    this._date = `${now.getFullYear()}年${now.getMonth()+1}月`
+    this._date = `${now.getFullYear()}年${now.getMonth()+1}月`;
+    this._pad = [...new Array(this.data[0].getDay()).keys()];
+    now.setHours(0, 0, 0, 0);
+    this.selectedDate = now.getTime();
   }
   navigate(evt) {
     const index = evt.currentTarget.dataset.index;
-    this.dispatchEvent(new CustomEvent('submit', {
-      detail: index
-    }));
+    this.selectedDate = parseInt(index)
   }
   _getDays(month, year) {
     return Array.from({
@@ -33,21 +40,29 @@ export class CustomDatePicker extends LitElement {
   }
   render() {
     return html`<div class="dialog">
-
   <div class="dialog-body">
-    <div style="grid-column:  1 / 3;">
+    <div class="nav pre">
+      <svg viewBox="0 0 24 24">
+        <path d="M15.422 16.594l-1.406 1.406-6-6 6-6 1.406 1.406-4.594 4.594z"></path>
+      </svg>
     </div>
     <div class="title">${this._date}</div>
-    <div style="grid-column:  6 / 8;">
+    <div class="nav next">
+      <svg viewBox="0 0 24 24">
+        <path d="M8.578 16.594l4.594-4.594-4.594-4.594 1.406-1.406 6 6-6 6z"></path>
+      </svg>
     </div>
     ${this._weeks .map((element,index)=>{
     return html`<div class="week" data-index="${index}">${element}</div>`;
     })}
-${this.data .map((element,index)=>{
-  return html`<div class="item" data-index="${index}" @click="${this.navigate}">${element.getDate()}</div>`;
-  })}
+    ${this._pad.map((element,index)=>{
+    return html`<div class="item"></div>`;
+    })}
+    ${this.data .map((element,index)=>{
+    console.log(element,this.selectedDate);
+    return html`<div class="item ${this.selectedDate===element.getTime()?'selected':''}" data-index="${element.getTime()}" @click="${this.navigate}">${element.getDate()}</div>`;
+    })}
   </div>
-  
 </div>
 <div class="overlay" @click="${this.close}">
 </div>`;
@@ -61,5 +76,5 @@ customElements.define('custom-date-picker', CustomDatePicker);
 <!--
 <script type="module" src="../components/custom-date-picker.js"></script>
 <custom-date-picker bind @submit=""></custom-date-picker>
-                                         -->
-                                     */
+-->
+*/

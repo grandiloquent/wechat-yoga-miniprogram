153aeab07a1870f7d6894f24e57268fa3c6b8153
grandiloquent <1785907865@qq.com>
2023-01-08 12:34:20 +0800

    更新

--- app/backend/js/image.js
+++ app/backend/js/image.js
@@ -20,10 +20,47 @@ async function downloadData() {
         if (response.status > 399 || response.status < 200) {
             throw new Error(`${response.status}: ${response.statusText}`)
         }
-        const results = await response.text();
-        console.log(results);
+        return await response.json();
     } catch (error) {
         console.log(error);
     }
 }
-downloadData();
+
+async function render() {
+    let results
+    try {
+        results = await downloadData();
+        renderLessons(line1, 32400, results);
+        renderLessons(line2, 70200, results);
+    } catch (error) {
+        console.log(error);
+    }
+    if (!results) return;
+}
+
+function renderLessons(element, start_time, results) {
+    element.insertAdjacentHTML('afterend',
+        [...new Array(7).keys()].map((value, index) => {
+            const obj = results.filter((value) => {
+                if (value.start_time !== start_time) {
+                    return false;
+                }
+                const date = new Date(value.date_time * 1000);
+                return (date.getDay() === 0 ? 7 : date.getDay()) === index + 1;
+            })
+            if (!obj.length) {
+                return `<div class="item">假日</div>`
+            } else {
+                return `<div class="item">
+            <div style="margin-bottom: 8px;">
+               ${obj[0].lesson_name}
+            </div>
+         <div style="font-size: 18px;">
+         ${obj[0].teacher_name}
+         </div>
+         </div>`
+            }
+        }).join(''));
+}
+
+render();

c1b9c1a0b1789a39a280c3a3038652c762435fdf
grandiloquent <1785907865@qq.com>
2023-04-27 16:24:46 +0800

    配置 postgres

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,36 @@
+use std::env;
+
+use deadpool_postgres::{ManagerConfig, Runtime};
+use tokio_postgres::NoTls;
+
 #[macro_use]
 extern crate rocket;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
+    let mut config = deadpool_postgres::Config::new();
+
+    config.host = Some(env::var("DB_HOST").expect("Can't specify"));
+    config.port = Some(
+        env::var("DB_PORT")
+            .expect("Can't specify")
+            .parse::<u16>()
+            .expect("Couldn't parse"),
+    );
+    config.password = Some(env::var("DB_PASSWORD").expect("Can't specify"));
+    config.dbname = Some("yoga".to_string());
+    config.user = Some("psycho".to_string());
+    config.manager = Some(ManagerConfig {
+        recycling_method: deadpool_postgres::RecyclingMethod::Fast,
+    });
+
     // https://rocket.rs/v0.4/guide/configuration/#rockettoml
     rocket::build()
+        .manage(
+            config
+                .create_pool(Some(Runtime::Tokio1), NoTls)
+                .expect("Can't create pool"),
+        )
         .mount("/", routes![])
         //.register("/", catchers![handlers::not_found::not_found])
         .launch()
@@ -12,4 +38,5 @@ async fn main() -> Result<(), rocket::Error> {
 
     Ok(())
 }
+
 

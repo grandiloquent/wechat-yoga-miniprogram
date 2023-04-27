de42bdb1b50f5e188db3ff73e7cdfef20d5aa64f
grandiloquent <1785907865@qq.com>
2023-04-27 15:59:32 +0800

    使用 Rocket.toml 配置服务器

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,34 +1,14 @@
-use rocket::{figment::Figment, log::LogLevel};
-
 #[macro_use]
 extern crate rocket;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    // let limits = Limits::default()
-    //     .limit("json", 3.mebibytes())
-    //     .limit("string", 3.mebibytes())
-    //     .limit("file", 5.gibibytes());
-
-    let figment = Figment::from(rocket::Config::default())
-        .merge((rocket::Config::ADDRESS, "127.0.0.1"))
-        .merge((rocket::Config::PORT, 8002))
-        //.merge((rocket::Config::TEMP_DIR, ""))
-        //.merge((rocket::Config::LIMITS, limits))
-        .merge((rocket::Config::LOG_LEVEL, LogLevel::Critical));
-
-    rocket::custom(figment)
-        .mount(
-            "/",
-            routes![
-                
-            ],
-        )
+    // https://rocket.rs/v0.4/guide/configuration/#rockettoml
+    rocket::build()
+        .mount("/", routes![])
         //.register("/", catchers![handlers::not_found::not_found])
         .launch()
         .await?;
 
     Ok(())
 }
-
-

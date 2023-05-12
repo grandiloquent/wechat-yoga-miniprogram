2635911146a84d69aa608eecb6b2e5b596879475
grandiloquent <1785907865@qq.com>
2023-05-12 15:44:53 +0800

    更新

--- miniprogram/pages/adminLesson/adminLesson.js
+++ miniprogram/pages/adminLesson/adminLesson.js
@@ -8,7 +8,32 @@ Page({
         indexs: ["今天", "明天", "近七日", "近半月"]
     },
     async onLoad(options) {
-        const id = options.id || 1271;
-        console.log(id);
-    }
+        this.data.id = options.id || 1271;
+        this.loadData();
+    },
+    async loadData() {
+        const lesson = await
+            new Promise((resolve, reject) => {
+                const url = `${app.globalData.host}/yoga/admin/lesson?id=${this.data.id}&openid=${app.globalData.openid}`
+                wx.request({
+                    url,
+                    success(res) {
+                        resolve(res.data);
+                    },
+                    fail(error) {
+                        reject();
+                    }
+                });
+            });
+        lesson.date = formatLessonDateTime(lesson);
+        this.setData({
+            lesson
+        })
+    },
 });
+function formatLessonDateTime(lesson) {
+    const date = new Date(lesson.date_time * 1000);
+    //  周${'日一二三四五六'[date.getDay()]} 
+    //  ${lesson.start_time / 3600 | 0}:${((lesson.start_time % 3600) / 60 | 0).toString().padStart(2, '0')}
+    return `${date.getMonth() + 1}月${date.getDate()}日${lesson.start_time < 43200 ? '上午' : '晚上'}`
+}

70f1d44c417e184419602910278d949f04b0109f
grandiloquent <1785907865@qq.com>
2023-05-14 00:36:12 +0800

    更新

---
+++ server/src/handlers/schedule.md
@@ -0,0 +1,60 @@
+```
+select pg_get_functiondef(oid) from pg_proc where proname = 'query_week_lessons';
+```
+
+```
+drop FUNCTION fn_query_week_lessons;
+``                
+
+```
+CREATE OR REPLACE FUNCTION fn_query_week_lessons()
+    RETURNS json
+    LANGUAGE plpgsql
+AS
+$function$
+DECLARE
+    seconds      int;
+    date_seconds int;
+    week         int;
+    date_times   int[];
+    obj          json;
+BEGIN
+    select floor(extract(epoch from now())) into seconds;
+    date_seconds = seconds - seconds % 86400 - 28800;
+    select extract(dow from now()) into week;
+    raise notice 'Date values: % = % = %',seconds,date_seconds,week;
+    if week = 0 then
+        date_seconds = date_seconds + 86400;
+    else
+        date_seconds = date_seconds - 86400 * (week - 1);
+    end if;
+    FOR i in 0..5
+        LOOP
+            date_times = array_append(date_times, date_seconds + 86400 * i);
+        end loop;
+    select json_agg(r) into obj
+    from (select course.start_time,
+                 course.end_time,
+                 course.date_time,
+                 l.name,
+                 c.name
+          from course
+                   join lesson l on l.id = course.lesson_id
+                   join coach c on course.teacher_id = c.id
+          where
+            --course.date_time >= 1665158400 and course.date_time <= (1665158400 + 86400* 6)
+              course.date_time = ANY (date_times)
+            and course.class_type = 4
+          order by date_time, start_time) r;
+    return obj;
+END
+$function$
+```
+
+```
+select * from fn_query_week_lessons()
+```
+                
+
+
+                
--- server/src/handlers/schedule.rs
+++ server/src/handlers/schedule.rs
@@ -1,4 +1,4 @@
-use crate::utils::data::query_json_with_params;
+use crate::utils::data::query_json;
 use deadpool_postgres::Pool;
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, RgbImage, Rgba};
@@ -10,33 +10,55 @@ use rusttype::{point, Font, Rect, Scale};
 use std::io::{Cursor, Write};
 use std::path::Path;
 #[get("/yoga/admin/schedule")]
-pub async fn admin_schedule(pool: &State<Pool>) -> Result<Vec<u8>, Status> {
-    // https://docs.rs/rusttype/0.9.2/rusttype/enum.Font.html
-    let bytes = include_bytes!("pattern.png") as &[u8];
+// Vec<u8>
+pub async fn admin_schedule(pool: &State<Pool>) -> Result<String, Status> {
+     match pool.get().await {
+        Ok(conn) => match query_json(&conn, "select * from fn_query_week_lessons()").await {
+            Ok(v) => {
+                return match String::from_utf8(v.0) {
+                    Ok(v) => Ok(v),
+                    Err(_) => Err(Status::InternalServerError),
+                };
+            }
+            Err(error) => {
+                println!("Error: {}", error);
+                Err(Status::InternalServerError)
+            }
+        },
+        Err(error) => {
+            println!("Error: {}", error);
+            Err(Status::InternalServerError)
+        }
+    }
+    /*let bytes = include_bytes!("pattern.png") as &[u8];
     let mut image = ImageReader::new(Cursor::new(bytes))
         .with_guessed_format()
         .unwrap()
         .decode()
         .unwrap();
+    // https://docs.rs/rusttype/0.9.2/rusttype/enum.Font.html
     let font = Vec::from(include_bytes!("PingFang.ttf") as &[u8]);
     let font = Font::try_from_vec(font).unwrap();
-    //https://docs.rs/rusttype/latest/rusttype/struct.VMetrics.html
-    let text = "欧阳老师";
-    draw_text(&font, &mut image, text,22.4,0,478);
+
+    
+    let text = "周一";
+    draw_text(&font, &mut image, text,36.4,0,186);
     let mut bytes: Vec<u8> = Vec::new();
     let _ = image.write_to(&mut Cursor::new(&mut bytes), image::ImageOutputFormat::Png);
     Ok(bytes)
+*/
 }
-// https://docs.rs/imageproc/latest/imageproc/drawing/fn.draw_text_mut.html
-// https://github.com/search?q=draw_text_mut%20path%3A*.rs&type=code
+
 // http://127.0.0.1:8002/yoga/admin/schedule
-// 28f32 
+// 28f32,0, 446,
+// 22.4,0,478
 fn draw_text(font: &Font, image: &mut DynamicImage, text: &str, font_size:f32,x: i32, y: i32) {
    
     let scale = Scale {
         x: font_size,
         y: font_size,
     };
+    // https://docs.rs/rusttype/latest/rusttype/struct.VMetrics.html
     let point = point(0.0, font.v_metrics(scale).ascent);
     let glyphs: Vec<Rect<i32>> = font
         .layout(text, scale, point)
@@ -48,11 +70,14 @@ fn draw_text(font: &Font, image: &mut DynamicImage, text: &str, font_size:f32,x:
     let max_y = glyphs.iter().map(|g| g.max.y).max().unwrap();
     let height = max_y - min_y;
     let width = last.x - first.x;
+
+    // https://docs.rs/imageproc/latest/imageproc/drawing/fn.draw_text_mut.html
+    // https://github.com/search?q=draw_text_mut%20path%3A*.rs&type=code
     draw_text_mut(
         image,
         Rgba([255u8, 255u8, 255u8, 255u8]),
         202i32 + 135 * x + (100 - width) / 2,
-        y, //446i32,
+        y, 
         scale,
         &font,
         text,

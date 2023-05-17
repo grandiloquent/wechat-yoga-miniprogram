fc01c0e9d5fcdabfe33b3bb9a0733c9b9df9c71f
grandiloquent <1785907865@qq.com>
2023-05-17 08:12:10 +0800

    更新

--- miniprogram/pkg/weixin.js
+++ miniprogram/pkg/weixin.js
@@ -1,5 +1,7 @@
-
 const shared=require("../utils/shared");
+const  encoding = require('../utils/encoding');
+const  TextDecoder = TextDecoder?TextDecoder:encoding.TextDecoder;
+const TextEncoder = TextEncoder?TextEncoder:encoding.TextEncoder;
 let wasm;
 
 const cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
@@ -336,11 +338,11 @@ function __wbg_adapter_59(arg0, arg1, arg2, arg3) {
     wasm.wasm_bindgen__convert__closures__invoke2_mut__h0d52a275242271dc(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
 }
 
-
 async function load(module, imports){
   const instance = await WXWebAssembly.instantiate(module, imports);
   return instance;
 }
+
 function getImports() {
     const imports = {};
     imports.wbg = {};
@@ -513,24 +515,23 @@ function finalizeInit(instance, module) {
     return wasm;
 }
 
-function initSync(module) {
+
+
+
+			async function init(){
+
+
     const imports = getImports();
+
 
     initMemory(imports);
 
-    if (!(module instanceof WebAssembly.Module)) {
-        module = new WebAssembly.Module(module);
-    }
-
-    const instance = new WebAssembly.Instance(module, imports);
-
-    return finalizeInit(instance, module);
-}
-
-async function init() {
-    const imports = getImports();
-    initMemory(imports);
     const { instance, module } = await load("/pkg/weixin_bg.wasm", imports);
+
     return finalizeInit(instance, module);
+
 }
+
+
+
 export default init;
